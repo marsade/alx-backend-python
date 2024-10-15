29bc7f06f541565ef61313e678a60ba3495f6370
mars <adenijifara@gmail.com>
2024-10-15 12:05:23 +0300

    Task complete

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 '''Async generator script'''
 import asyncio
+import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     '''Async generator function'''
     for i in range(10):
         asyncio.sleep(1)
-        yield i
+        yield random.uniform(0, 10)
