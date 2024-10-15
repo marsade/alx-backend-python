9dd5d9d9f5b8d4732526736c56b2293ac58e7ca0
mars <adenijifara@gmail.com>
2024-10-15 12:44:51 +0300

    Update

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,12 +9,12 @@ async def measure_runtime() -> float:
     '''Measures the execution time of an async generator function
 
     Returns: float, representing the execution time in seconds'''
-    start_time = time.time()
+    start_time = time.perf_counter()
     run = await asyncio.gather(
         async_comp(),
         async_comp(),
         async_comp(),
         async_comp(),
     )
-    end_time = time.time()
+    end_time = time.perf_counter()
     return end_time - start_time
