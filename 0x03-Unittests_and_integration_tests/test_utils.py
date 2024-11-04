d2fc7104fdd2188b1ce33c19b64bce3c3f38a6ed
mars <adenijifara@gmail.com>
2024-11-04 16:42:13 +0200

    Task 0 complete

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env/python3
+'''Testing Utils.py'''
+
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+
+class TestAccessNestedMap(unittest.TestCase):
+    '''Test access_nested_map function'''
+
+    def test_access_nested_map(self):
+        '''Test access_nested_map function with valid inputs'''
+        @parameterized.expand([
+            ({'a': 1}, ('a',), 1),
+            ({'a': {'b': 2}}, ('a', ), {'b': 2}),
+            ({'a': {'b': 2}}, ('a', 'b')),
+        ])
+        def test_access_nested_map_valid(self, nested_map, path, expected):
+            '''Test access_nested_map function with valid inputs'''
+            self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
