3730436c43314bce5adc5321f613b80543c802f1
mars <adenijifara@gmail.com>
2024-11-04 16:48:23 +0200

    Update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,17 +7,15 @@ from parameterized import parameterized
 
 class TestAccessNestedMap(unittest.TestCase):
     '''Test access_nested_map function'''
+    @parameterized.expand([
+        ({'a': 1}, ('a',), 1),
+        ({'a': {'b': 2}}, ('a', ), {'b': 2}),
+        ({'a': {'b': 2}}, ('a', 'b')),
+    ])
 
-    def test_access_nested_map(self):
+    def test_access_nested_map(self, nested_map, path, expected):
         '''Test access_nested_map function with valid inputs'''
-        @parameterized.expand([
-            ({'a': 1}, ('a',), 1),
-            ({'a': {'b': 2}}, ('a', ), {'b': 2}),
-            ({'a': {'b': 2}}, ('a', 'b')),
-        ])
-        def test_access_nested_map_valid(self, nested_map, path, expected):
-            '''Test access_nested_map function with valid inputs'''
-            self.assertEqual(access_nested_map(nested_map, path), expected)
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
 
 if __name__ == '__main__':
