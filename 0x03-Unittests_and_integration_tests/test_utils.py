6992a08dfffdaccdf731d61b297101c81e1bd18b
Mars Adeniji <adenijifara@gmail.com>
2025-02-06 15:56:25 +0200

    Add exceptions test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,9 +2,10 @@
 '''Testing Utils.py'''
 
 import unittest
-from typing import Mapping, Sequence
+from typing import Mapping, Sequence, Type
 from parameterized import parameterized
 from utils import access_nested_map
+
 
 class TestAccessNestedMap(unittest.TestCase):
     '''Test access_nested_map function'''
@@ -13,14 +14,32 @@ class TestAccessNestedMap(unittest.TestCase):
         ({'a': {'b': 2}}, ('a', ), {'b': 2}),
         ({'a': {'b': 2}}, ('a', 'b'), 2),
     ])
-
     def test_access_nested_map(self, nested_map: Mapping,
-                                path: Sequence, expected: int) -> None:
+                               path: Sequence, expected: int) -> None:
         '''Test access_nested_map function with valid inputs
 
         Args:
             nested_map (Mapping): A nested dictionary
-            path (Sequence): A sequence of keys representing a path to the value
+            path (Sequence): A sequence of keys representing
+            a path to the value
             expected (int): The expected value at the given path'''
         res = access_nested_map(nested_map, path)
         self.assertEqual(res, expected)
+
+    @parameterized.expand([
+        ('empty_map', {}, ('a'), KeyError),
+        ('wrong_path', {'a': 1}, ('a', 'b'), KeyError)
+    ])
+    def test_access_nested_map_exception(self, name, nested_map: Mapping,
+                                         path: Sequence,
+                                         expected_exception: Type[Exception]):
+        '''Test access_nested_map function with invalid inputs
+
+        Args:
+            name (str): Test case name
+            nested_map (Mapping): A nested dictionary
+            path (Sequence): A sequence of keys representing
+            a path to the value
+            expected_exception (type): The expected exception type'''
+        with self.assertRaises(expected_exception):
+            res = access_nested_map(nested_map, path)
