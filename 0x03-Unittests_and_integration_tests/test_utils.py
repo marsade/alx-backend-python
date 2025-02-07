50eecffd02df9fb5dbd26b2264e9d0846c486d59
Mars Adeniji <adenijifara@gmail.com>
2025-02-07 17:33:55 +0200

    Task 2 complete

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 '''Testing Utils.py'''
-
 import unittest
+from unittest.mock import patch
 from typing import Mapping, Sequence, Type
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -47,3 +47,20 @@ class TestAccessNestedMap(unittest.TestCase):
 
 class TestGetJson(unittest.TestCase):
     '''Test get_json function'''
+    @parameterized.expand([
+        ('http://example.com', {'payload': True}),
+        ('https://holberton.io', {'payload': True}),
+    ])
+    def test_get_json(self, url: str, expected: Mapping) -> None:
+        '''Test get_json function with valid inputs
+
+        Args:
+            url (str): A URL
+            expected (Mapping): The expected JSON payload
+        '''
+        with patch('requests.get') as mock_get:
+            mock_get.return_value.json.return_value = expected
+            res = get_json(url)
+
+            mock_get.assert_called_once_with(url)
+            self.assertEqual(res, expected)
