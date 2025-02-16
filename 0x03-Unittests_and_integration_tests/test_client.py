d7251cf42c54239c0915a43be94a94393a8ea5b8
Mars Adeniji <adenijifara@gmail.com>
2025-02-16 17:21:08 +0200

    Add test for has_license method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -57,3 +57,11 @@ class TestGithubOrgClient(unittest.TestCase):
 
             mock.assert_called_once()
             mock_json.assert_called_once()
+
+    @parameterized.expand([
+        ({'license': {'key': 'my_license'}}, 'my_license', True),
+        ({'license': {'key': 'other_license'}}, 'my_license', False)
+    ])
+    def test_has_license(self, repo, license_key, expected_license):
+        res = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(res, expected_license)
