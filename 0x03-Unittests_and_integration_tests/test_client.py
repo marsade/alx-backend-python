7b03c0ca37ae8a088582a5cb69e2b8bfb5c44f7d
Mars Adeniji <adenijifara@gmail.com>
2025-02-16 17:09:09 +0200

    Complete test for public repos method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -16,8 +16,8 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_org(self, input, mock_json):
         '''Test Org method'''
-        expected_result = {"name": input, "repos_url":
-                           f"https://api.github.com/orgs/{input}/repos"}
+        expected_result = {'name': input, 'repos_url':
+                           f'https://api.github.com/orgs/{input}/repos'}
         mock_json.return_value = expected_result
         test = GithubOrgClient(input)
         result = test.org
@@ -28,7 +28,7 @@ class TestGithubOrgClient(unittest.TestCase):
 
     def test_public_repos_url(self):
         known_payload = {'name': 'google',
-                         'repos_url': 'https://github.com/orgs'}
+                         'repos_url': 'https://github.com/orgs/google'}
 
         with patch('client.GithubOrgClient.org',
                    new_callable=PropertyMock) as mock:
@@ -36,3 +36,24 @@ class TestGithubOrgClient(unittest.TestCase):
             test = GithubOrgClient('google')
             self.assertEqual(test._public_repos_url,
                              known_payload['repos_url'])
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_json):
+        json_res = [{'name': 'google', 'id': 123433, 'gists_url':
+                     'https://api.github.com/orgs/google/repos'},
+                    {'name': 'kratos', 'id': 123433, 'gists_url':
+                     'https://api.github.com/orgs/kratos/repos'}
+                    ]
+        mock_json.return_value = json_res
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock:
+            pub_repos_payload = 'https://github.com/orgs/google'
+            mock.return_value = pub_repos_payload
+            test = GithubOrgClient('google')
+            res = test.public_repos()
+
+            repos = [repo['name'] for repo in json_res]
+            self.assertEqual(res, repos)
+
+            mock.assert_called_once()
+            mock_json.asserT_called_once()
