9bc58c00b190a7d63dd9d778bf7d12c120d7094d
Mars Adeniji <adenijifara@gmail.com>
2025-06-21 16:00:55 +0300

    Add docstring to function

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,6 +4,7 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    '''Return the sum of a list of floats'''
     sum = 0
     for i in input_list:
         sum = sum + i
