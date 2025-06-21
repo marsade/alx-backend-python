8f027530d4e7db5313658b32e85b74d2773b1986
Mars Adeniji <adenijifara@gmail.com>
2025-06-21 17:37:17 +0300

    Task complete

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env pyton3
+'''Complex annotations - Iterable'''
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
