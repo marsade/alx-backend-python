25c2722d37039df0c8cbdc7c1a77c7a35ad0d497
Mars Adeniji <adenijifara@gmail.com>
2025-06-21 18:01:07 +0300

    Add dosctring to function

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,4 +4,5 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''Return a list of tuples with the length of each element in lst'''
     return [(i, len(i)) for i in lst]
