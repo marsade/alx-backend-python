064c97f5364b931c3292ffcfcb0616bf535b2cd5
Mars Adeniji <adenijifara@gmail.com>
2025-06-21 16:11:09 +0300

    Use typing module import

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 '''Mixed list annotations'''
+from typing import Union
 
 
-def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+def sum_mixed_list(mxd_lst: list[Union[int | float]]) -> float:
     '''Return the sum of a mixed list of integers and floats.'''
     sum = 0
     for i in mxd_lst:
