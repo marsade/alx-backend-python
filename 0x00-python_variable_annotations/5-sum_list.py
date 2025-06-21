cc8d71e60d21a3184521f63d2f4ed7ced26749eb
Mars Adeniji <adenijifara@gmail.com>
2025-06-21 15:58:01 +0300

    Use imports

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 '''List of floats annotations'''
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     sum = 0
     for i in input_list:
         sum = sum + i
