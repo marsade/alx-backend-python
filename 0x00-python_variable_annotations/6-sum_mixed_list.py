81812ab42c545c5fccb6e7afc769acdf7e283898
Mars Adeniji <adenijifara@gmail.com>
2025-06-21 16:19:30 +0300

    Import List module

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 '''Mixed list annotations'''
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     '''Return the sum of a mixed list of integers and floats.'''
     sum = 0
     for i in mxd_lst:
