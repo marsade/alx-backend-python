9b9f1aa9ea1bde3c09381fcabdeb332a3d54658b
Mars Adeniji <adenijifara@gmail.com>
2025-06-21 16:07:29 +0300

    Add docstrings to function

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,7 @@
 
 
 def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+    '''Return the sum of a mixed list of integers and floats.'''
     sum = 0
     for i in mxd_lst:
         sum = sum + i
