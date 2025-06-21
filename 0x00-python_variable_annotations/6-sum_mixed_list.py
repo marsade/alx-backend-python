eff2c26e6917f2e47f8f85fe2c95b2711b677398
Mars Adeniji <adenijifara@gmail.com>
2025-06-21 16:15:06 +0300

    Change \ to ,

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 from typing import Union
 
 
-def sum_mixed_list(mxd_lst: list[Union[int | float]]) -> float:
+def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
     '''Return the sum of a mixed list of integers and floats.'''
     sum = 0
     for i in mxd_lst:
